aebaf3bf06a9952ad8e56f990c3c3ff618ba351e
natispena <penanatalia56@gmail.com>
2021-12-29 12:05:09 -0500

    modelo usuarios y turnos

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField(max_length=100, verbose_name="nombre")
+    apellidos=models.CharField(max_length=100)
+    telefono=models.IntegerField()
+    identificacion=models.CharField(max_length=100)
+    staff=models.CharField(max_length=100)
+    foto= models.ImageField(upload_to='imagenes/', null=True)
+
+class Turnos(models.Model):
+    id = models.AutoField(primary_key=True)
+    numero = models.IntegerField()
+    hora=models.TimeField()
+    estado=models.CharField(max_length=100)
+    Usuario=models.CharField(max_length=100)
